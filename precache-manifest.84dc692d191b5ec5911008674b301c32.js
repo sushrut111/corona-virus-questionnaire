772ee36d4e3c045def12038f49437ff88cee4cf8
Sushrut Kasture <kasturesushrut@gmail.com>
2020-03-25 09:49:09 +0530

    Updates

---
+++ precache-manifest.84dc692d191b5ec5911008674b301c32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5efc43e08ce77781376281dae4d99c",
+    "url": "/corona-virus-questionnaire/index.html"
+  },
+  {
+    "revision": "1872ca81c88623bd0bf4",
+    "url": "/corona-virus-questionnaire/static/css/2.aeed1b96.chunk.css"
+  },
+  {
+    "revision": "1db78dcda91b471ceeeb",
+    "url": "/corona-virus-questionnaire/static/css/main.8e1740df.chunk.css"
+  },
+  {
+    "revision": "1872ca81c88623bd0bf4",
+    "url": "/corona-virus-questionnaire/static/js/2.bfa51cf9.chunk.js"
+  },
+  {
+    "revision": "faa3785e2552619a937289b03994fc2a",
+    "url": "/corona-virus-questionnaire/static/js/2.bfa51cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db78dcda91b471ceeeb",
+    "url": "/corona-virus-questionnaire/static/js/main.5ff82574.chunk.js"
+  },
+  {
+    "revision": "e1ea7d5946a74ec817a0",
+    "url": "/corona-virus-questionnaire/static/js/runtime-main.72806453.js"
+  }
+]);
