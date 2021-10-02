df89af45f83ade0c9d5d6e653ed85fe11297a57e
Devenkapoor7303 <deven.kapoor1999@gmail.com>
2021-10-01 23:42:46 -0800

    Back Button is not visible in Homepage

--- src/app/components/languageSelector.js
+++ src/app/components/languageSelector.js
@@ -4,20 +4,36 @@ import "../../App.css";
 
 function LanguageSelector(props) {
   const { languages, languageChange, goHome, part, language } = props;
+  const renderContent=()=>{
+    if(part===0){
+      return (
+        <div className="column">
+          <div onClick={goHome} className="backButton">
+            <i class="fas fa-arrow-circle-left fa-4x clickable"></i>
+          </div>
+          <h1 className= 'title heading1'>
+            GoCorona
+          </h1>
+          <h2 className="subtitle heading2">Corona go!!</h2>
+        </div>
+      );
+    }else{
+      return(
+        <div className="column">
+          <h1 className= 'title '>
+            GoCorona
+          </h1>
+          <h2 className="subtitle">Corona go!!</h2>
+        </div>
+      );
+    }
+  }
   return (
     <section className="hero is-info is-bold">
       <div className="hero-body">
         <div className="container">
           <div className="columns">
-            <div className="column">
-              <div onClick={goHome} className="backButton">
-                <i class="fas fa-arrow-circle-left fa-4x clickable"></i>
-              </div>
-              <h1 className="title heading1">
-                GoCorona
-              </h1>
-              <h2 className="subtitle heading2">Corona go!!</h2>
-            </div>
+            {renderContent()}
             <div>
               {part != 4 ? (
                 <div className="select is-rounded is-pulled-right">
